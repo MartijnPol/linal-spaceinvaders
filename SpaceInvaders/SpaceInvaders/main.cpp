ceada0c96a7da180cbbe02c17a31c04bd67c7f65
Thom van de Pas <thomvandepas@hotmail.com>
2019-01-11 14:05:14 +0100

    Auto rotating around every axis

--- SpaceInvaders/SpaceInvaders/main.cpp
+++ SpaceInvaders/SpaceInvaders/main.cpp
@@ -23,7 +23,6 @@ int main(int argc, char * argv[])
 	const auto spacing = 25;
 	const auto width = 30;
 	const auto height = 20;
-	const auto degrees = 1.0f;
 
 	auto timer = TimerFacade{};
 	auto graphics = GraphicsFacade{ width,height,spacing };
@@ -34,18 +33,17 @@ int main(int argc, char * argv[])
 										 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f }
 	);
 
-	auto target = Matrix<float>(3, 4, { 0, 0, 5, 5,
-										0, 5, 5, 0,
-										1, 1, 1, 1 });
+	auto target = Matrix<float>(4, 4, { 0.0f, 0.0f, 5.0f, 5.0f,
+										0.0f, 5.0f, 5.0f, 0.0f,
+										1.0f, 1.0f, 1.0f, 1.0f,
+										0.0f, 0.0f, 0.0f, 0.0f });
 
-	Vector3D<float> translate_vector({ 2, 2 }, 2.0f, 2.0f, 2.0f);
+	Vector3D<float> translate_vector({ 2, 2 }, 1.0f, 1.0f, 1.0f);
 
 	const auto max_scale_factor = 0.5f;
 	auto scale_factor = 0.0f;
 	auto reversed = false;
 
-	auto translate_x_factor = 0.0f;
-	auto translate_y_factor = 0.0f;
 	auto speed = 0.1f;
 
 	SDL_Event event;
@@ -57,12 +55,17 @@ int main(int argc, char * argv[])
 			graphics.clear();
 			graphics.draw_coordinate_system();
 
+			Vector3D<float> scale_vector({ 2, 2 }, scale_factor, scale_factor, scale_factor);
+
+			translate_vector += Vector3D<float>({ 2,2 }, 5.0f, 3.0f, 1.0f);
+
 			auto rotated_rocket = rotate(rocket, translate_vector);
 			auto translated_rocket = translate(rotated_rocket, translate_vector);
 
 			graphics.draw_matrix(translated_rocket, GraphicsFacade::preset_color::blue);
 
-			auto scaled_target = scale(target, scale_factor, scale_factor);
+			auto rotated_target = rotate(target, translate_vector);
+			auto scaled_target = scale(rotated_target, scale_vector);
 			graphics.draw_matrix(scaled_target, GraphicsFacade::preset_color::red);
 
 			SDL_PollEvent(&event);
