a7005c588ff63393a6ab359be08922f6968cd729
Thom van de Pas <thomvandepas@hotmail.com>
2019-01-11 11:33:26 +0100

    Fixed definition of pi

--- SpaceInvaders/SpaceInvaders/Math.h
+++ SpaceInvaders/SpaceInvaders/Math.h
@@ -1,13 +1,17 @@
 #ifndef MATH_MATH_H
 #define MATH_MATH_H
 
+#ifndef PI
+#define PI 3.14159265358979323846
+#endif
+
 namespace math
 {
 	struct Math
 	{
 		static float degrees_to_radius(const float degrees)
 		{
-			return degrees * (M_PI / 180.0f);
+			return degrees * (PI / 180.0f);
 		}
 	};
 }
